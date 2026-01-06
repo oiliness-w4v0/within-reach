4dbc7892690ce1653797854650c471d04dfc1264
@油腻_w4v0 <16615812538@163.com>
2026-01-06 09:55:44 +0800

    Fix Vite capitalization in renderer comment

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,5 @@
 /**
- * This file will automatically be loaded by vite and run in the "renderer" context.
+ * This file will automatically be loaded by Vite and run in the "renderer" context.
  * To learn more about the differences between the "main" and the "renderer" context in
  * Electron, visit:
  *
